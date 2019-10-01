d97a99fe7c5b8a2008b2e1488d30fd8e7403f59c
Mikaela Karstens <mikaelawesthoff@gmail.com>
2019-10-01 16:31:55 -0400

    work from 10/1/19

--- First-Shot-Rep-Fail-Model.R
+++ First-Shot-Rep-Fail-Model.R
@@ -1,2 +1,10 @@
 # Repeat Failure Model
 
+library(foreign) # download .dta files for Stata < 13
+library(haven) # download .dta files for Stata 13+
+library(sampleSelection) # Package containing heckit for heckprob substitute
+
+data <- read_dta("Deterring_TC.dta") #Uses haven
+
+names(data) #print variable names to make sure everything read in alright.
+
